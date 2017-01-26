f0086e418fd22e2a07e581de57ef7d56b3eca646
Akshit Sahani <akshit_sahani@hotmail.com>
2017-01-26 17:18:22 -0500

    everything works. YAY

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,20 +3,22 @@ def translate(input)
   words = input.split(" ")
   final_words = []
   words.each do |word|
-  chars = word.each_char.to_a
-  final = []
-  if vowels.include?(chars[0])
-    final_words << (chars.join) + "ay"
-  else
-    i = 0
-    # string.each do |char|
+    chars = word.each_char.to_a
+    final = []
+    if vowels.include?(chars[0])
+      final_words << (chars.join) + "ay"
+    else
+      i = 0
       while !vowels.include?(chars[i])
-        # string.each do |char|
           final << chars[i]
           i += 1
       end
-        final_words << (chars.join.slice!(i..-1) + final.join + "ay")
+      if chars[i] == "u" && final[-1] == "q"
+        final << chars[i] #adding u to the end of final. We have (q, u) for the final
+        i += 1
+      end
+      final_words << (chars.join.slice!(i..-1) + final.join + "ay")
+    end
   end
-end
   return final_words.join(" ")
 end
